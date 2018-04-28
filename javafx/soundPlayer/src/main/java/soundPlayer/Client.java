f0ce90db44d429414ca5bf1788b88d2264abe695
olaven <olavsundfoer@gmail.com>
2018-04-28 19:34:14 +0200

    started refactoring Client, having a weird bug

--- javafx/soundPlayer/src/main/java/soundPlayer/Client.java
+++ javafx/soundPlayer/src/main/java/soundPlayer/Client.java
@@ -8,6 +8,8 @@ import java.io.File;
 import javafx.application.Application; 
 import javafx.stage.Stage; 
 
+import javafx.event.ActionEvent;
+
 import javafx.scene.Scene;  
 import javafx.scene.layout.GridPane; 
 import javafx.scene.control.Label;
@@ -16,50 +18,40 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer; 
 import javafx.scene.media.MediaView; 
 
-import javafx.event.ActionEvent; 
-
-
 /**
  * javafx client to present tracks to
  * the user and let the user play them. 
  */
 public class Client extends Application
 {
-    private Button chooseFolderButton; 
-    ArrayList<String> acceptedExtensions = new ArrayList<>(Arrays.asList("mp3", "flac"));
-    GridPane layout = new GridPane(); 
-
     public void start(Stage stage)
     {
-        chooseFolderButton= new Button("Choose Folder");
-        chooseFolderButton.setOnAction(event -> {
-            File directory = DirectoryReader.openDirectoryDialogue(stage);
-            updateList(directory, acceptedExtensions); 
+        GridPane gridPane = new GridPane(); 
+
+        Button chooseDirectoryButton = new Button ("Choose directory"); 
+        chooseDirectoryButton.setOnAction(ActionEvent e -> {
+            DirectoryReader.openDirectoryDialogue(stage); 
         }); 
 
-        layout.add(chooseFolderButton, 2, 0); 
-
-        layout.setGridLinesVisible(true);
-
-        stage.setScene(new Scene(layout, 250, 500)); 
+        Scene scene = new Scene(gridPane, 400, 400); 
+        stage.setScene(scene); 
+        stage.setTitle("Music player"); 
         stage.show(); 
     }
-    /**
-     * update GUI-list based on given directory
-     */
-    private void updateList(File directory, ArrayList<String> acceptedExtensions)
-    {
-        ArrayList<File> files = DirectoryReader.getFilesFromDirectory(directory, acceptedExtensions); 
-        //print files 
-        for(File file : files)
-        {
-            System.out.println(file.getName());
-                        //string -> file -> string : REFACTOR 
-            Media media = new Media(file.getPath()); 
-            MediaPlayer mediaPlayer = new MediaPlayer(media); 
-            MediaView mediaView = new MediaView(mediaPlayer);  
 
-            layout.add(mediaView, 0, 0, 2, 2); 
-        }
+    /**
+     * Updates the list  
+     */
+    public void updateList()
+    {
+        //update files visible to user 
+    }
+
+    /**
+     * Updates the media players track 
+     */
+    public void updatePlayer()
+    {
+        //update the list visible to the user 
     }
 }
