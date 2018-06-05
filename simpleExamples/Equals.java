9246f34ccb8896dfb1f7daccc138cb36d9c4e00f
olaven <olavsundfoer@gmail.com>
2018-06-05 14:30:04 +0200

    endret equals-eksempel

--- simpleExamples/Equals.java
+++ simpleExamples/Equals.java
@@ -3,18 +3,40 @@
  */
 public class Equals 
 {
+
+    public static void main(String[] args)
+    {
+        Equals e = new Equals("En verdi", "Verdi to"); 
+        Equals e2 = new Equals("En verdi", "Verdi to"); 
+        Equals b = new Equals("Annen verdi", "Annen verdi 2"); 
+
+        if(e.equals(e2))
+        {
+            System.out.println("De ligner"); 
+        }
+    }
+
     private String field1; 
     private String field2; 
 
+    public Equals(String field1, String field2)
+    {
+        this.field1 = field1; 
+        this.field2 = field2; 
+    }
+
     @Override 
     public boolean equals(Object object)
-    {   
-        if(object == this) return true; 
-        if(object == null || object.getClass != getClass()) return false; 
+    {
+        if(this == object) return true; 
+        if(this == null || getClass() != object.getClass()) return false; 
 
-        // My idea of what should equal equality 
-        if(getField1().equals(object.getField1()) && getField2().equals(object.getField2()))
-            return true; 
+        //Equals equals = (Equals) object; 
+        object = (Equals) object; // FUNGERER IKKE 
+ 
+
+        if(equals.getField1().equals(getField1()) && equals.getField2().equals(getField2())) return true; 
+        return false; 
     }
 
     public String getField1()
