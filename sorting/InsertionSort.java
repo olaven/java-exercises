23f6be9b936dae18214b4685da30383e83f96f15
olaven <olavsundfoer@gmail.com>
2018-09-06 19:10:09 +0200

    made InsertionSort a bit more compact

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -17,41 +17,15 @@ public class InsertionSort {
         System.out.println(); 
     }
 
-    public static <T extends Comparable> T[] sort(T[] array) {
-        for (int i = 0; i < array.length; i++) {
-            int current = i; 
-            while((current - 1) > 0 && less(array[current], array[current - 1])) {
-                exchange(current, (current - 1), array); 
-                current --; 
+    public static<T extends Comparable> T[] sort(T[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = i; 
+            while(key > 0 && array[key - 1].compareTo(array[key]) >= 1) {
+                T temp = array[key]; 
+                array[key] = array[key - 1]; 
+                array[key - 1] = temp; 
             }
         }
-
-        return array; 
-    }
-
-    /**
-     * @param first first paramter
-     * @param second second parameter
-     * @return true if first is less, false if not 
-     */
-    public static boolean less(Comparable first, Comparable second) {
-        boolean less = first.compareTo(second) < 0; 
-        return less; 
-    }
-
-    /**
-     * Exchanges elements at given positions
-     * @param i index of first element
-     * @param j index of second element
-     * @param array array to exchange inside 
-     * @return the array with exchanged arrays 
-     */
-    public static<T extends Comparable> T[] exchange(int i, int j, T[] array) {
-        T temp = array[i]; 
-        array[i] = array[j]; 
-        array[j] = temp; 
-
         return array; 
     }
 }
-
