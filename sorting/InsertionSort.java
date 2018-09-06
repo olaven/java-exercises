c72a59708818081ce2c5e9c891226b2e8fa0b08f
olaven <olavsundfoer@gmail.com>
2018-09-06 19:04:47 +0200

    Insertion sorr works

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,35 +1,57 @@
-import com.sun.org.apache.xalan.internal.xsltc.compiler.util.CompareGenerator;
-
+/**
+ * InsertionSort
+ * @author olaven
+ */
+/**
+ * InsertionSort
+ */
 public class InsertionSort {
+
     public static void main(String[] args) {
-        Integer[] array = { 1, 4, 103, 3, 6, 2, 6, 5, 66, 23, 1 };
-        array = (Integer[]) BubbleSort.sort(array);
+        Integer[] array = {1,53,2,12,101,42, 100, 2}; 
+        array = (Integer[]) InsertionSort.sort(array); 
 
-        for (Integer num : array) {
-            System.out.print(num + " ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " "); 
         }
-
-        System.out.println();
+        System.out.println(); 
     }
 
-    public static Comparable[] sort(Comparable[] array) {
-        if(array.length <= 1) 
-            return array; 
-
-        for (int i = 1; i < array.length; i++) {
-            // loop back again and swap elemens 
-            for(int j = i; j >= 0; j--) {
-                if(array[i].compareTo(array[j]) <= 1) 
-                    array = InsertionSort.exchange(i, j, array); 
+    public static <T extends Comparable> T[] sort(T[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int current = i; 
+            while((current - 1) > 0 && less(array[current], array[current - 1])) {
+                exchange(current, (current - 1), array); 
+                current --; 
             }
         }
-        return null; 
+
+        return array; 
     }
 
-    private static Comparable[] exchange(int i, int j, Comparable[] array) {
-        Comparable temp = array[i]; 
+    /**
+     * @param first first paramter
+     * @param second second parameter
+     * @return true if first is less, false if not 
+     */
+    public static boolean less(Comparable first, Comparable second) {
+        boolean less = first.compareTo(second) < 0; 
+        return less; 
+    }
+
+    /**
+     * Exchanges elements at given positions
+     * @param i index of first element
+     * @param j index of second element
+     * @param array array to exchange inside 
+     * @return the array with exchanged arrays 
+     */
+    public static<T extends Comparable> T[] exchange(int i, int j, T[] array) {
+        T temp = array[i]; 
         array[i] = array[j]; 
         array[j] = temp; 
+
         return array; 
     }
 }
+
